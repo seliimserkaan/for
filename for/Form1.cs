178bfa30910d14b7c04eeab9788443169e830402
Selim <Selim@DESKTOP-PVUL0ER>
2022-10-21 10:08:41 +0300

    Proje dosyası ekle.

---
+++ for/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace @for
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                for (int i = 10; i <= 110; i+=10)
+                {
+                    listBox1.Items.Add(i);
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
